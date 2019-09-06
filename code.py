7c738ac30638ebb6781851ac8efcfb5b12266113
David McCoy <davidmccoy@Davids-MacBook-Pro.local>
2019-09-06 00:47:43 -0700

    added more code to code.py

--- code.py
+++ code.py
@@ -1,1 +1,20 @@
 x = 1 and x = 3
+
+
+def testJackpot(arr):
+    ans = False
+    first = arr[0]
+    count = 0
+    for i in range(len(arr)):
+        if first == arr[i]:
+            count += 1
+    if count == len(arr):
+        ans = True
+
+    return ans
+
+print(testJackpot(["$", "$$", "$$$", "$$$$"]))
+print(testJackpot(["&&", "&", "&&&", "&&&&"]))
+print(testJackpot(["&", "&", "$a", "&"]))
+print(testJackpot(["@@", "@@", "@@", "@@"]))
+print(testJackpot(["Nadeem","Nadeem"])) # ➞ false
